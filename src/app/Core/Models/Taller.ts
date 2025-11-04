b8f7f54d634635c7ddfb45cfad121bc06a7c486d
LeandroCiriaco <leandrodanteciriaco@gmail.com>
2025-11-04 10:30:07 -0300

    creacion de modelos y un poco de form

---
+++ src/app/Core/Models/Taller.ts
@@ -0,0 +1,13 @@
+import { LookupItem } from "./ENUM";
+import { User } from "./User";
+import { Vehiculo } from "./Vehiculo";
+
+export interface Taller{
+    id: string,
+    Especialidad: LookupItem,
+    NombreTaller: string,
+    Encargado: User,
+    Vehiculos: Vehiculo[],
+    Direccion: string
+}
+
