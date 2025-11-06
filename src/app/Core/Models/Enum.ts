1edd90da363cd54af80237b2f2e4410f619ce847
frantribu <franmartinmdq@gmail.com>
2025-11-06 20:33:51 -0300

    cambio

--- src/app/Core/Models/Enum.ts
+++ src/app/Core/Models/Enum.ts
@@ -3,18 +3,18 @@
  * Ejemplo: Role, Combustion, TipoAuto, etc.
  */
 export interface LookupItem {
-  id: string;
-  name: string;
-}
+    id: string;
+    name: string;
+  }
+  
+  // Interfaces específicas (opcionales pero útiles para tipado)
+  export interface Role extends LookupItem {}
+  export interface Combustion extends LookupItem {}
+  export interface TipoAuto extends LookupItem {}
+  export interface TipoMoto extends LookupItem {}
+  export interface Especialidad extends LookupItem {}
 
-// Interfaces específicas (opcionales pero útiles para tipado)
-export interface Role extends LookupItem {}
-export interface Combustion extends LookupItem {}
-export interface TipoAuto extends LookupItem {}
-export interface TipoMoto extends LookupItem {}
-export interface Especialidad extends LookupItem {}
-
-export interface ArchivoVehiculo extends File{
+  export interface ArchivoVehiculo extends File{
   urlLocal ?: string
   rutaSimulada ?: string
 }
