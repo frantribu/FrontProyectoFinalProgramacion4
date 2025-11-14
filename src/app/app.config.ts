f22a28bcbaa515a0dcce45a7ce7f3fbc54549b5d
gonzaa <gonzalo.diaz91218gmail.com>
2025-11-14 11:24:53 -0300

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,23 +4,16 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { UserServiceService } from './Core/Services/UserService/user-service.service';
 import { TallerServiceService } from './Core/Services/Taller/TallerService/taller-service.service';
-<<<<<<< HEAD
-=======
 import { ClientService } from './Core/Services/ClientService/client.service';
 import { RolesService } from './Core/Services/RolService/roles.service';
->>>>>>> origin/CON-37-ABM-de-Cliente
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideHttpClient(withFetch()),
   { provide: UserServiceService },
-<<<<<<< HEAD
-  { provide: TallerServiceService}
-=======
   { provide: TallerServiceService},
   { provide: ClientService },
   { provide: RolesService }
->>>>>>> origin/CON-37-ABM-de-Cliente
   ]
 };
