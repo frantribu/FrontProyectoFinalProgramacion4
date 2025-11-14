de521e613529ac80f5866d84d187c43370b1f34f
Bigfa12 <milrayas7@gmail.com>
2025-11-14 15:44:29 -0300

    A

--- src/app/Core/Services/Taller/EspecialidadService/especialidad-service.ts
+++ src/app/Core/Services/Taller/EspecialidadService/especialidad-service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Especialidad } from '../../../Models/ENUM';
+import { Especialidad } from '../../../Models/Enum';
 
-import
+
 
 @Injectable({
   providedIn: 'root',
