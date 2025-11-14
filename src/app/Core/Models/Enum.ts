fb6d474b406cd2543e45f8323ff3bc5a889edc03
Bigfa12 <milrayas7@gmail.com>
2025-11-14 15:53:12 -0300

    s

--- src/app/Core/Models/Enum.ts
+++ src/app/Core/Models/Enum.ts
@@ -3,19 +3,13 @@
  * Ejemplo: Role, Combustion, TipoAuto, etc.
  */
 export interface LookupItem {
-  id: string;
-  name: string;
-}
-
-// Interfaces específicas (opcionales pero útiles para tipado)
-export interface Role extends LookupItem { }
-export interface Combustion extends LookupItem { }
-export interface TipoAuto extends LookupItem { }
-export interface TipoMoto extends LookupItem { }
-export interface Especialidad extends LookupItem { }
-
-
-export interface ArchivoVehiculo extends File {
-  urlLocal?: string
-  rutaSimulada?: string
-}
+    id: string;
+    name: string;
+  }
+  
+  // Interfaces específicas (opcionales pero útiles para tipado)
+  export interface Role extends LookupItem {}
+  export interface Combustion extends LookupItem {}
+  export interface TipoAuto extends LookupItem {}
+  export interface TipoMoto extends LookupItem {}
+  export interface Especialidad extends LookupItem {}
